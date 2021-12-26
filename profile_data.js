b17ebb7c281cf1f3b050d5b89d2ebf8c24ab8096
volkswg <volkswg@hotmail.com>
2021-12-27 01:24:59 +0700

    updated profile data

--- profile_data.js
+++ profile_data.js
@@ -1,61 +1,61 @@
 exports.profile_list = {
   base_profile: [
+    // {
+    //   // 9
+    //   email: "sy.siriwanjang@gmail.com",
+    //   emailForm: "sy.siriwanjang@gmail.com",
+    //   password: "s230940j",
+    //   fullname: "Siriwan Yoocharoen",
+    //   idcard: "1102002819243",
+    //   phoneNo: "0957213235",
+    // },
+    // {
+    //   // 9
+    //   email: "pepo.kiyoko@gmail.com",
+    //   emailForm: "pepo.kiyoko@gmail.com",
+    //   password: "Sj@230940",
+    //   fullname: "Chonipha Yujaroen",
+    //   idcard: "1102001335030",
+    //   phoneNo: "0909039966",
+    // },
+    // {
+    //   // 9
+    //   email: "higuysusu1@gmail.com",
+    //   emailForm: "higuysusu1@gmail.com",
+    //   password: "0830161522",
+    //   fullname: "Panida Sandot",
+    //   idcard: "1100701214381",
+    //   phoneNo: "0802217340",
+    // },
+    // {
+    //   // 9
+    //   email: "higuysusu2@gmail.com",
+    //   emailForm: "higuysusu2@gmail.com",
+    //   password: "0830161522",
+    //   fullname: "Siriphon Oiucharoen",
+    //   idcard: "3102102127081",
+    //   phoneNo: "0859078142",
+    // },
+    // {
+    //   // 9
+    //   email: "higuysusu3@gmail.com",
+    //   emailForm: "higuysusu3@gmail.com",
+    //   password: "0830161522",
+    //   fullname: "Patthama Knongchum",
+    //   idcard: "1102000192413",
+    //   phoneNo: "0802217325",
+    // },
     {
-      //
-      email: "sy.siriwanjang@gmail.com",
-      emailForm: "sy.siriwanjang@gmail.com",
-      password: "s230940j",
-      fullname: "Siriwan Yoocharoen",
-      idcard: "1102002819243",
-      phoneNo: "0957213235",
-    },
-    {
-      //
-      email: "pepo.kiyoko@gmail.com",
-      emailForm: "pepo.kiyoko@gmail.com",
-      password: "Sj@230940",
-      fullname: "Chonipha Yujaroen",
-      idcard: "1102001335030",
-      phoneNo: "0909039966",
-    },
-    {
-      //
-      email: "higuysusu1@gmail.com",
-      emailForm: "higuysusu1@gmail.com",
-      password: "0830161522",
-      fullname: "Panida Sandot",
-      idcard: "1100701214381",
-      phoneNo: "0846946229",
-    },
-    {
-      //
-      email: "higuysusu2@gmail.com",
-      emailForm: "higuysusu2@gmail.com",
-      password: "0830161522",
-      fullname: "Siriphon Oiucharoen",
-      idcard: "3102102127081",
-      phoneNo: "0859078142",
-    },
-    {
-      //
-      email: "higuysusu3@gmail.com",
-      emailForm: "higuysusu3@gmail.com",
-      password: "0830161522",
-      fullname: "Patthama Knongchum",
-      idcard: "1102000192413",
-      phoneNo: "0948548607",
-    },
-    {
-      //
+      // 11
       email: "higuysusu4@gmail.com",
       emailForm: "higuysusu4@gmail.com",
       password: "0830161522",
       fullname: "Chainon Yucharoen",
       idcard: "1102001335021",
-      phoneNo: "0841183456",
+      phoneNo: "0802216140",
     },
     {
-      //
+      // 11
       email: "higuysusu6@gmail.com",
       emailForm: "higuysusu6@gmail.com",
       password: "0830161522",
@@ -64,17 +64,17 @@ exports.profile_list = {
       phoneNo: "0990544008",
     },
     {
-      //
+      // 11
       email: "susuhiguy5@gmail.com",
       password: "0830161522",
       //----------------------------
-      // fullname: "นันทิรา ฝั้นแก้ว",
-      // idcard: "1101500904438",
-      // phoneNo: "0816712130",
+      fullname: "นันทิรา ฝั้นแก้ว",
+      idcard: "3509901485321",
+      phoneNo: "0816712130",
       //----------------------------
-      fullname: "อลินดา มัตติตานัง",
-      idcard: "1101500915961",
-      phoneNo: "0802212212",
+      // fullname: "อลินดา มัตติตานัง",
+      // idcard: "1101500915961",
+      // phoneNo: "0802212212",
       //----------------------------
       // emailForm: "sent_kj@hotmail.com",
       // fullname: "Kittiphan Chareansuk",
@@ -82,39 +82,40 @@ exports.profile_list = {
       // phoneNo: "0827124976",
       //----------------------------
     },
-    // { //
-    //   email: "Susuhiguy7@gmail.com",
-    //   password: "0830161522",
-    //   //----------------------------
-    //   // fullname: "นิพนธ์ ฝั้นแก้ว",
-    //   // idcard: "1101500966310",
-    //   // phoneNo: "0946411331",
-    //   //----------------------------
-    //   fullname: "ณัฐสุภา สงมา",
-    //   idcard: "1101500966310",
-    //   phoneNo: "0643156506",
-    //   //----------------------------
-    //   // emailForm: "detcizz@gmail.com",
-    //   // fullname: "Ittidet Srivisut",
-    //   // idcard: "1200101716661",
-    //   // phoneNo: "0875355850",
-    //   //----------------------------
-    // },
+    {
+      // 11
+      email: "susuhiguy7@gmail.com",
+      password: "0830161522",
+      //----------------------------
+      fullname: "นิพนธ์ ฝั้นแก้ว",
+      idcard: "3509901489784",
+      phoneNo: "0946411331",
+      //----------------------------
+      // fullname: "ณัฐสุภา สงมา",
+      // idcard: "1101500966310",
+      // phoneNo: "0643156506",
+      //----------------------------
+      // emailForm: "detcizz@gmail.com",
+      // fullname: "Ittidet Srivisut",
+      // idcard: "1200101716661",
+      // phoneNo: "0875355850",
+      //----------------------------
+    },
   ],
   other_profile: [
     // {
-    //   // 9,7
+    //   // 9
     //   email: "Elijahhh98@gmail.com",
     //   emailForm: "Wanidaonkhreng@gmail.com",
     //   password: "0830161522",
     //   //----------------------------
-    //   // fullname: "Wanida Onkhreng",
-    //   // idcard: "1101500904438",
-    //   // phoneNo: "0991028043",
+    //   fullname: "Wanida Onkhreng",
+    //   idcard: "1101500904438",
+    //   phoneNo: "0991028043",
     //   //----------------------------
     // },
     // {
-    //   // 8,7
+    //   // 9
     //   email: "photoonly.memory@gmail.com",
     //   password: "s230940j",
     //   //----------------------------
@@ -130,7 +131,7 @@ exports.profile_list = {
     //   //----------------------------
     // },
     // {
-    //   // 8,7
+    //   // 9
     //   email: "metharoma@gmail.com",
     //   emailForm: "Thanatad20@gmail.com",
     //   password: "0830161522",
@@ -139,7 +140,7 @@ exports.profile_list = {
     //   phoneNo: "0826909361",
     // },
     // {
-    //   // 9,7
+    //   // 9
     //   email: "mtsuolei@gmail.com",
     //   emailForm: "angudjikung@hotmail.com",
     //   password: "0830161522",
@@ -148,7 +149,7 @@ exports.profile_list = {
     //   phoneNo: "0817160465",
     // },
     // {
-    //   // 9,7
+    //   // 9
     //   email: "poungteiklahan@gmail.com",
     //   emailForm: "prinzessinbkk@gmail.com",
     //   password: "0830161522",
@@ -157,7 +158,7 @@ exports.profile_list = {
     //   phoneNo: "0894528269",
     // },
     // {
-    //   // 9,7
+    //   // 9
     //   email: "sunan.uxach@gmail.com",
     //   emailForm: "Meta301998@gmail.com",
     //   password: "0830161522",
@@ -166,7 +167,7 @@ exports.profile_list = {
     //   phoneNo: "0925297408",
     // },
     // {
-    //   // 9,7
+    //   // 9
     //   email: "songrayson554@gmail.com",
     //   emailForm: "6755asiavariety@gmail.com",
     //   password: "0830161522",
@@ -175,7 +176,7 @@ exports.profile_list = {
     //   phoneNo: "0939716474",
     // },
     // {
-    //   // 9,7
+    //   // 9
     //   email: "nnoah1917@gmail.com",
     //   emailForm: "praew.thidarat2543@gmail.com",
     //   password: "0830161522",
@@ -184,7 +185,7 @@ exports.profile_list = {
     //   phoneNo: "0655530580",
     // },
     // {
-    //   // 9,7
+    //   // 9
     //   email: "Oliverewd90@gmail.com",
     //   emailForm: "Mrson_n_4@hotmail.com",
     //   password: "0830161522",
@@ -193,7 +194,7 @@ exports.profile_list = {
     //   phoneNo: "0918232373",
     // },
     // {
-    //   // 9,7
+    //   // 9
     //   email: "Lucassa3456@gmail.com",
     //   emailForm: "songmanutsupa@gmail.com",
     //   password: "0830161522",
@@ -202,7 +203,7 @@ exports.profile_list = {
     //   phoneNo: "0643156506",
     // },
     // {
-    //   // 9,7
+    //   // 8
     //   email: "masonny896@gmail.com",
     //   emailForm: "eid.nattanicha3001@gmail.com ",
     //   password: "0830161522",
@@ -211,7 +212,7 @@ exports.profile_list = {
     //   phoneNo: "0640029162",
     // },
     // {
-    //   // 9,7
+    //   // 8
     //   email: "ethanethany787@gmail.com",
     //   emailForm: "Prinzessinbkk@gmail.com",
     //   password: "0830161522",
@@ -220,7 +221,7 @@ exports.profile_list = {
     //   phoneNo: "0894528269",
     // },
     // {
-    //   // 9,7
+    //   // 8
     //   email: "mateomate7593@gmail.com",
     //   emailForm: "loveoil159@hotmail.com",
     //   password: "0830161522",
@@ -229,7 +230,7 @@ exports.profile_list = {
     //   phoneNo: "0873308721",
     // },
     // {
-    //   //10,7
+    //   // 8
     //   email: "jamesjames4320@gmail.com",
     //   emailForm: "t.katatorn@gmail.com",
     //   password: "0830161522",
@@ -238,7 +239,7 @@ exports.profile_list = {
     //   phoneNo: "0614562686",
     // },
     // {
-    //   //10,7
+    //   // 8
     //   email: "loganlogan3232@gmail.com",
     //   emailForm: "arunee5665@gmail.com",
     //   password: "0830161522",
@@ -247,7 +248,7 @@ exports.profile_list = {
     //   phoneNo: "0930145665",
     // },
     // {
-    //   //10,7
+    //   // 8
     //   email: "aidenn9879@gmail.com",
     //   emailForm: "huasio2016@gmail.com",
     //   password: "0830161522",
@@ -256,7 +257,7 @@ exports.profile_list = {
     //   phoneNo: "0624253563",
     // },
     // {
-    //   //10,7
+    //   // 8
     //   email: "dogsssd08@gmail.com",
     //   emailForm: "doremon191@gmail.com",
     //   password: "0830161522",
@@ -265,7 +266,7 @@ exports.profile_list = {
     //   phoneNo: "0829897711",
     // },
     // {
-    //   //10,7
+    //   // 8
     //   email: "jj0113140@gmail.com",
     //   emailForm: "Tanawat@pmtc.ac.th",
     //   password: "0830161522",
@@ -274,7 +275,7 @@ exports.profile_list = {
     //   phoneNo: "0911346608",
     // },
     // {
-    //   //10,7
+    //   // 8
     //   email: "jj188298@gmail.com",
     //   emailForm: "Thayarun.inp@gmail.com",
     //   password: "0830161522",
@@ -283,7 +284,7 @@ exports.profile_list = {
     //   phoneNo: "0829471936",
     // },
     // {
-    //   //10,7
+    //   // 8
     //   email: "llevi8240@gmail.com",
     //   emailForm: "Jamjuree.sueb@bumail.net",
     //   password: "0830161522",
